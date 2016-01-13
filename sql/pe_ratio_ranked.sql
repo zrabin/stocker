d6d7d7f767cc0bc69958f36fce7f5c44c7042c1a
Chris Fischer <fischer.christopher@gmail.com>
2016-01-13 15:11:16 -0500

    added queries

---
+++ sql/pe_ratio_ranked.sql
@@ -0,0 +1,8 @@
+SELECT company.symbol, company.name, financialdata.pe_ratio_ttm 
+FROM company inner join financialdata 
+ON company.id = financialdata.company_id 
+WHERE financialdata.pe_ratio_ttm > 0 
+ORDER BY financialdata.pe_ratio_ttm 
+DESC;
+
+
