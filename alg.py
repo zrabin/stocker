5be73412ea2acdfa45850a84755e84af2eff2428
Chris Fischer <fischer.christopher@gmail.com>
2016-01-25 22:40:28 -0500

    added some bs for future earnings, maybe not legic

--- alg.py
+++ alg.py
@@ -18,6 +18,34 @@ def tech_50_ranked_pe_ratio():
     
     return score
         
+def tech_50_ranked_eps_estimate_qtr():
+    sql = (d.Company
+                .select(d.Company.name, d.Company.symbol, d.FinancialData.eps_estimate_qtr)
+                .join(d.FinancialData, on=(d.Company == d.FinancialData.company_id))
+                .where((d.Company.sector == 'Technology') & (d.FinancialData.eps_estimate_qtr != None))
+                .order_by(d.FinancialData.pe_ratio_ttm)
+            )
+    
+    score = {}
+    for rank, company in enumerate(sql):
+        score.update({company.symbol : rank})
+    
+    return score
+
+def tech_50_ranked_price():
+    sql = (d.Company
+                .select(d.Company.name, d.Company.symbol, d.FinancialData.ask)
+                .join(d.FinancialData, on=(d.Company == d.FinancialData.company_id))
+                .where((d.Company.sector == 'Technology') & (d.FinancialData.eps_estimate_qtr != None))
+                .order_by(d.FinancialData.ask)
+            )
+    
+    score = {}
+    for rank, company in enumerate(sql):
+        score.update({company.symbol : rank})
+    
+    return score
+
 def tech_50_ranked_roa():
     sql = (d.Company
             .select(d.Company.name, d.Company.symbol, d.FinancialData.return_on_assets)
@@ -40,6 +68,31 @@ def magic_50():
     for i in pe.keys():
         if i in roa:
             combined = pe[i] + roa[i]
+            score.update({i : combined})
+   
+    sorted_score = sorted(score.items(), key=operator.itemgetter(1))
+
+    return sorted_score
+
+def magic_50_future():
+    pe = tech_50_ranked_pe_ratio()
+    future_eps = tech_50_ranked_eps_estimate_qtr()
+    price = tech_50_ranked_price()
+    roa = tech_50_ranked_roa()
+
+    f_pe = {}
+    for i in price.keys():
+        if i in future_eps:
+            try:
+                future_pe = price[i] / future_eps[i]
+                f_pe.update({i : future_pe})
+            except:
+                continue
+    
+    score = {}
+    for i in f_pe.keys():
+        if i in roa:
+            combined = f_pe[i] + roa[i]
             score.update({i : combined})
    
     sorted_score = sorted(score.items(), key=operator.itemgetter(1))
