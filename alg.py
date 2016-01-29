6753c5b0cb31faf7a4a1bc326fa5766f4aa25ec0
Chris Fischer <fischer.christopher@gmail.com>
2016-01-29 13:14:55 -0500

    abstracted out alg module

--- alg.py
+++ alg.py
@@ -4,7 +4,7 @@ import os
 import database as d
 import operator 
 
-def tech_50_magic_form():
+def tech_magic_formula():
     # Score company pe_ttm
     tech_pe_ttm = (d.Company
                 .select(d.Company.name, d.Company.symbol, d.FinancialData.pe_ratio_ttm)
@@ -41,7 +41,7 @@ def tech_50_magic_form():
 
     return sorted_score
         
-def magic_form():
+def magic_formula():
     # Score company pe_ttm
     pe_ttm = (d.Company
                 .select(d.Company.name, d.Company.symbol, d.FinancialData.pe_ratio_ttm)
@@ -82,47 +82,3 @@ def magic_form():
 
     return sorted_score
         
-
-
-def magic_50():
-    pe = tech_50_ranked_pe_ratio()
-    roa = tech_50_ranked_roa()
-
-    score = {}
-    for i in pe.keys():
-        if i in roa:
-            combined = pe[i] + roa[i]
-            score.update({i : combined})
-   
-    sorted_score = sorted(score.items(), key=operator.itemgetter(1))
-
-    return sorted_score
-
-def magic_50_future():
-    pe = tech_50_ranked_pe_ratio()
-    future_eps = tech_50_ranked_eps_estimate_qtr()
-    price = tech_50_ranked_price()
-    roa = tech_50_ranked_roa()
-
-    f_pe = {}
-    for i in price.keys():
-        if i in future_eps:
-            try:
-                future_pe = price[i] / future_eps[i]
-                f_pe.update({i : future_pe})
-            except:
-                continue
-    
-    score = {}
-    for i in f_pe.keys():
-        if i in roa:
-            combined = f_pe[i] + roa[i]
-            score.update({i : combined})
-   
-    sorted_score = sorted(score.items(), key=operator.itemgetter(1))
-
-    return sorted_score
-
-companies = magic_form()
-for i in companies[:30]:
-    print i 
