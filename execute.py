d75c9fe6fa45244fee26919c633d6662343969c2
Chris Fischer <fischer.christopher@gmail.com>
2016-02-19 12:42:07 -0500

    modified to accept list or str arguments for getCompany method

--- execute.py
+++ execute.py
@@ -6,7 +6,7 @@ from alg import Alg
 #data = rankings.getRankings(strategy="magic_formula_ttm")
 
 data1 = Alg().getRankings('magic_formula_ttm')
-data2 = Alg().getCompany('AAPL')
+data2 = Alg().getCompany(['AAPL', 'SSTK'])
 
 print tabulate(data1, headers="keys")
 print data2 
