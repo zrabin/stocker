5af026f2a3da5c2c6481360a445fce78773f83df
root <chris@fischer.nyc>
2016-05-23 10:19:41 -0400

    added new script file

--- webapp.py
+++ webapp.py
@@ -100,4 +100,4 @@ def create_app(configfile=None):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=4444, debug=True) 
+    app.run(host='0.0.0.0', port=80, debug=True) 
